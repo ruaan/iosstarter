99528da79165d728af0a94713b7bd13702760fcd
Ruaan <ruaan@tangentsolutions.co.za>
2019-12-12 09:15:54 +0530

    Initial Commit

---
+++ starterios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  starterios
+//
+//  Created by Ruaan Erasmus on 12/12/19.
+//  Copyright © 2019 Ruaan Erasmus. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
